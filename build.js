0fb2d8a48acf39a44ff7b19892a53e794f8ad8a7
Josh Evenson <jevenson@bhtp.com>
2018-03-05 16:41:36 -0600

    autoprefixer, browsersync, environments

--- build.js
+++ build.js
@@ -1,37 +1,58 @@
-const Metalsmith = require('metalsmith'),
+const
+    metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
     layouts = require('metalsmith-layouts'),
     discoverPartials = require('metalsmith-discover-partials'),
     collections = require('metalsmith-collections'),
     permalinks = require('metalsmith-permalinks'),
     sass = require('metalsmith-sass'),
-    Handlebars = require('handlebars'),
-    CircularJSON = require('circular-json');
+    handlebars = require('handlebars'),
+    autoprefixer = require('metalsmith-autoprefixer'),
+    browsersync = require('metalsmith-browser-sync'),
+    circularJSON = require('circular-json');
 
-Handlebars.registerHelper('json', function(context) {
-    return CircularJSON.stringify(context);
+const environment = process.argv[2] || 'dev';
+
+handlebars.registerHelper('json', function(context) {
+    return circularJSON.stringify(context);
 });
 
-Metalsmith(__dirname)
-    .source('src')
-    .destination('dist')
-    .use(collections({
-        pages: {
-            pattern: 'content/pages/*.md'
-        },
-        articles: {
-            pattern: 'content/articles/*.md',
-            sortBy: 'date'
-        }
-    }))
-    .use(markdown())
-    .use(permalinks({
-        pattern: ':collections/:title'
-    }))
-    .use(discoverPartials({
-        directory: 'layouts/partials',
-        pattern: /\.hbs$/
-    }))
-    .use(sass())
-    .use(layouts())
-    .build(function (err) { if(err) console.log(err) })
+const pipeline =
+    metalsmith(__dirname)
+        .source('src')
+        .destination('dist')
+        .use(collections({
+            pages: {
+                pattern: 'content/pages/*.md'
+            },
+            articles: {
+                pattern: 'content/articles/*.md',
+                sortBy: 'date'
+            }
+        }))
+        .use(markdown())
+        .use(permalinks({
+            pattern: ':collections/:title'
+        }))
+        .use(discoverPartials({
+            directory: 'layouts/partials',
+            pattern: /\.hbs$/
+        }))
+        .use(sass())
+        .use(autoprefixer())
+        .use(layouts());
+
+    if (environment === 'dev') {
+        pipeline
+            .use(browsersync({
+                server: "dist",
+                files: [
+                    "src/**/*.md",
+                    "src/**/*.scss",
+                    "layouts/**/*.hbs"
+                ]
+            }));
+    }
+
+    pipeline
+        .build(function (err) { if(err) console.log(err) })
--- src/index.md
+++ src/index.md
@@ -3,3 +3,7 @@ title: Home
 layout: home.hbs
 ---
 Whatever content we want on our front page...
+
+And more!
+
+Even more!
